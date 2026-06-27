742012b2bd474cfd66c3b4898f0815aaa18ec6ce
arunkoli2102-collab <arunkoli2102@gmail.com>
2026-06-27 20:38:06 +0530

    Create Project1_Hello.py

---
+++ Project1_Hello.py
@@ -0,0 +1,6 @@
+# AI Foundation for Engineers - Project 1
+# My First Python File on GitHub
+
+name = "Arun Kumar"
+print(f"Hello! Welcome to AI Foundation.")
+print("This is my first GitHub commit.")
